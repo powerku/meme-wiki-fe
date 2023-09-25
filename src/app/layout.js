3d99a250bd4cd28325df54b372dc1eab79e73d37
griffin <griffin.ku@kakaocorp.com>
2023-09-26 08:34:10 +0900

    add: layout 설정 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div id="wrap" className="m-0 mx-auto h-full bg-white" style={{
+        minWidth: '320px',
+        maxWidth: '420px'
+      }}>
+        {children}</div>
+      </body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return <h1>Hello Meme Wiki FE</h1>
+  return (
+        <h1>Hello Meme Wiki FE<</h1>
+  )
 }
+
