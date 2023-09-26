fd2789cbd23ef3fd94f664af9640d721b7df8aac
griffin <griffin.ku@kakaocorp.com>
2023-09-26 10:51:38 +0900

    fix: title, description 변경

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Header from '../components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,11 +13,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+
       <div id="wrap" className="m-0 mx-auto h-full bg-white" style={{
         minWidth: '320px',
         maxWidth: '420px'
       }}>
-        {children}</div>
+          <Header/>
+          <div className="conatiner p-16">
+              {children}
+          </div>
+      </div>
       </body>
     </html>
   )
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { signIn, signOut, useSession } from "next-auth/react"
+export default function Header({children}) {
+    return (
+        <div className="header fixed top-0 left-0 right-0 mx-auto my-0 flex px-2" style={{
+            minWidth: '320px',
+            maxWidth: '420px'
+        }}>
+            <div>Meme Wiki</div>
+            <a className="ml-auto">Login</a>
+        </div>
+    )
+}
