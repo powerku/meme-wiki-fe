f5371755deae5954424b986312566c273fda3267
griffin <griffin.ku@kakaocorp.com>
2023-09-26 11:20:43 +0900

    chore: eslint prettier 설정 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,33 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Header from '../components/Header'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Header from '../components/Header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Meme Wiki FE',
   description: 'Meme Wiki FE',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
 
-      <div id="wrap" className="m-0 mx-auto h-full bg-white" style={{
-        minWidth: '320px',
-        maxWidth: '420px'
-      }}>
-          <Header/>
+        <div
+          id="wrap"
+          className="m-0 mx-auto h-full bg-white"
+          style={{
+            minWidth: '320px',
+            maxWidth: '420px',
+          }}
+        >
+          <Header />
           <div className="conatiner p-16">
-              {children}
+            {children}
           </div>
-      </div>
+        </div>
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,3 @@
 export default function Home() {
-  return (
-        <h1>Hello Meme Wiki FE</h1>
-  )
+  return <h1>Hello Meme Wiki FE</h1>;
 }
-
